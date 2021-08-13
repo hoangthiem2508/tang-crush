101278668e64f9ed9f11f6588d574cd8767c48e0
hoangthiem2508 <88839255+hoangthiem2508@users.noreply.github.com>
2021-08-13 09:37:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const CONFIG = {
-    introTitle: 'Trang ơi!',
+    introTitle: 'Hi !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao cậu cứ đi nhầm đường
     Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Bấm vào đây đi ạ :3',
     title: 'Phải chăng cậu đã thích tớ ngay từ cái nhìn đầu tiên 😙',
-    desc: 'cậu có yêu tớ honggg nà ? ',
+    desc: 'Em yêu anh hong bé ơi ? ',
     btnYes: 'có chứ, yêu nhiều lắm <33',
-    btnNo: 'Không bao giờ :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
+    btnNo: 'Không bao giờ ',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu anh zậy ? <3',
     btnReply: 'Gửi cho Thiêm <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Chờ đến lúc ra hn rồi làm ny tớ nhá!',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Anh biết mà 🥰. nhưng àm hỏng có yêu em :)',
+    messDesc: 'heheheheheeheheheheehehehehehe',
 }
